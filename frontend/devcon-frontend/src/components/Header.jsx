859be4a4165911d1128eb65cac8ccaa119f8a683
bitMan <mrterm404@gmail.com>
2024-08-21 19:04:04 +0530

    feat: nav

---
+++ frontend/devcon-frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+// src/components/Header.jsx
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container container-fluid d-flex">
+    <div>
+        <a className="navbar-brand fs-3 fw-semibold" href="#">Devcon</a>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</nav>
+        </header>
+    )
+}
+
+export default Header
--- frontend/devcon-frontend/src/main.jsx
+++ frontend/devcon-frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import App from './App.jsx'
 import './index.css'
 
