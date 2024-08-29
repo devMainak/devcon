7cf7d60555c5cc61155e6ff5ea5f94891b2fd1d3
bitMan <mrterm404@gmail.com>
2024-08-29 12:13:09 +0530

    style: avatar changed

--- frontend/devcon-frontend/src/components/user/staticUserSlice.js
+++ frontend/devcon-frontend/src/components/user/staticUserSlice.js
@@ -7,31 +7,31 @@ export const staticUserSlice = createSlice({
         user: {
             name: "Elliot Alderson",
             username: "mrrobot",
-            userImageUrl: "https://media.istockphoto.com/id/1288129985/vector/missing-image-of-a-person-placeholder.jpg?s=612x612&w=0&k=20&c=9kE777krx5mrFHsxx02v60ideRWvIgI1RWzR1X4MG2Y=",
+            userImageUrl: "https://i.pinimg.com/564x/5c/13/28/5c132893d1aff7bc10e2489a22d3ac8e.jpg",
             userImageAvatars: [
                 {
                     id: 1,
-                    url: "https://i.pinimg.com/564x/e3/fc/71/e3fc715b352f6b606cc31d5329cc1737.jpg"
+                    url: "https://i.pinimg.com/564x/6c/00/ba/6c00bae75be1f3ce284eb647ca7d3cf3.jpg"
                 },
                 {
                     id: 2,
-                    url: "https://i.pinimg.com/564x/68/e7/60/68e760fd6fb9e79a8460a16f446411b2.jpg"
+                    url: "https://i.pinimg.com/564x/eb/f4/23/ebf42327ff1ece8ea0e95e94314bd76a.jpg"
                 },
                 {
                     id: 3,
-                    url: "https://i.pinimg.com/564x/21/2a/4b/212a4bf2b1b3c8e6d104c899323c7437.jpg"
+                    url: "https://i.pinimg.com/564x/fc/63/a6/fc63a64c7c05412e9c51cc4ee4be12a1.jpg"
                 },
                 {
                     id: 4,
-                    url: "https://i.pinimg.com/564x/a1/4d/24/a14d2497f060e2937b8778797a294103.jpg"
+                    url: "https://i.pinimg.com/564x/e1/a8/0b/e1a80b393c40c441c26433150f121cd8.jpg"
                 },
                 {
                     id: 5,
-                    url: "https://i.pinimg.com/564x/4a/97/13/4a9713120af046efc922f500f28f707e.jpg"
+                    url: "https://i.pinimg.com/564x/f3/33/87/f333877bbfd1abfb7a345e571f0d4b9c.jpg"
                 },
                 {
                     id: 6,
-                    url: "https://i.pinimg.com/564x/58/bb/d0/58bbd0dc01ba27609a19730938ef23c5.jpg"
+                    url: "https://i.pinimg.com/564x/03/14/ee/0314ee84a32d506d4fd9d7af46cc6bbd.jpg"
                 }
             ],
             profileBio: "Are you an One or a Zero?",
