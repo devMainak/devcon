f19532524c042b463f260c5d755f36cb57cdf820
bitMan <mrterm404@gmail.com>
2024-11-16 19:39:19 +0530

    refactor: logi logic updated

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -7,10 +7,10 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 // Register user
 exports.register = async (req, res) => {
-  const { name, username, password } = req.body;
+  const { name, username, email, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ name, username, password: hashedPassword });
+    const newUser = new User({ name, username, email, password: hashedPassword });
     const savedUser = await newUser.save();
     res.status(201).json({ message: "User registered", user: savedUser });
   } catch (error) {
@@ -25,7 +25,7 @@ exports.login = async (req, res) => {
     const user = await User.findOne({ username });
     if (user && (await bcrypt.compare(password, user.password))) {
       const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
-      res.status(200).json({ message: "Login successful", token });
+      res.status(200).json({ message: "Login successful", token, user });
     } else {
       res.status(401).json({ error: "Invalid credentials" });
     }
