871b124b143174fbf75517f2dbb64fc36043f5e6
bitMan <mainak.das@infomaticae.co.in>
2024-12-21 14:37:30 +0530

    refactor: cookie config updated

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -54,7 +54,7 @@ exports.login = async (req, res) => {
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
       res
