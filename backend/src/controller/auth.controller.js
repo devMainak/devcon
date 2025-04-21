ea804e7f393117f40dc561b15ae5d1f2db8e6509
hackersGambit <mrterm404@gmail.com>
2025-04-21 17:03:25 +0530

    fix: cookie settings

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -57,7 +57,7 @@ exports.login = async (req, res) => {
       res.cookie("refresh_token", refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "none",
+        sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
 
