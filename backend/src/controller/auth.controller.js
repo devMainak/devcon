8dbaa59188427de54cad51b87296ac6d69be0d37
hackersGambit <mrterm404@gmail.com>
2025-04-21 19:44:15 +0530

    fix: refresh token

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -57,7 +57,7 @@ exports.login = async (req, res) => {
       res.cookie("refresh_token", refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
 
